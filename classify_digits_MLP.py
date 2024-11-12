7b65c3f87e658de6817f52a1de8dc02f27eb3a9c
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-11-12 21:36:59 +0300

    Initial commit

---
+++ classify_digits_MLP.py
@@ -0,0 +1,26 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+plt.rcParams["axes.titlesize"] = 16
+plt.rcParams["axes.labelsize"] = 14
+plt.rcParams["image.cmap"] = "gray"
+
+from tensorflow.keras.datasets import fashion_mnist
+
+SEED_VALUE = 42
+
+random.seed(SEED_VALUE)
+np.random.seed(SEED_VALUE)
+tf.random.set_seed(SEED_VALUE)
+
